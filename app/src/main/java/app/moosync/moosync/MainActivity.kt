67ddf46ef47c797d2bc27ee3e35f665bf11fc7a3
Ovenoboyo <ovenoboyo@gmail.com>
2022-10-01 18:35:20 +0530

    Use databinding in main activity

--- app/src/main/java/app/moosync/moosync/MainActivity.kt
+++ app/src/main/java/app/moosync/moosync/MainActivity.kt
@@ -2,6 +2,10 @@ package app.moosync.moosync
 
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.databinding.DataBindingUtil
+import app.moosync.moosync.databinding.ActivityMainBinding
 import app.moosync.moosync.ui.base.BaseMainActivity
 import app.moosync.moosync.utils.db.repository.SongRepository
 import app.moosync.moosync.utils.helpers.AudioScanner
@@ -13,10 +17,16 @@ import kotlinx.coroutines.launch
 
 class MainActivity : BaseMainActivity() {
 
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var binding: ActivityMainBinding
+
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setToolbar()
+        setNavigationDrawer()
 
         PermissionManager(this).requestPermission {
             // TODO: Decide which scope would be better
@@ -30,9 +40,30 @@ class MainActivity : BaseMainActivity() {
         }
     }
 
+    private fun setToolbar() {
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        with(supportActionBar!!) {
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    private fun setNavigationDrawer() {
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.nav_open, R.string.nav_close)
+        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+    }
+
     override fun onDestroy() {
         Log.d("TAG", "onDestroy: Destroying main activity")
         getMediaRemote()?.release()
         super.onDestroy()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
