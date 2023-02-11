13f063c3dd8d1f330a6425903542200b177168d4
Ovenoboyo <ovenoboyo@gmail.com>
2023-02-11 18:02:14 +0530

    Setup nav controller

--- app/src/main/java/app/moosync/moosync/MainActivity.kt
+++ app/src/main/java/app/moosync/moosync/MainActivity.kt
@@ -3,6 +3,10 @@ package app.moosync.moosync
 import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import app.moosync.moosync.databinding.ActivityMainBinding
 import app.moosync.moosync.ui.base.BaseMainActivity
 import app.moosync.moosync.ui.handlers.BottomSheetHandler
@@ -35,19 +39,33 @@ class MainActivity : BaseMainActivity() {
                 Log.d("TAG", "onCreate: added songs")
             }
         }
+
+        val navController =
+            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+        binding.themedBottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _: NavController, navDestination: NavDestination, _: Bundle? ->
+            binding.appBarMain.songsListHeader.title.text = navDestination.label
+        }
     }
 
     private fun setupOverlayPlayers() {
         val behaviour = BottomSheetBehavior.from(binding.bottomSheet.standardBottomSheet)
-        val bottomSheetHandler = BottomSheetHandler(this, binding.bottomSheet, binding.themedBottomNavigationView)
+        val bottomSheetHandler =
+            BottomSheetHandler(this, binding.bottomSheet, binding.themedBottomNavigationView)
         bottomSheetHandler.setupBottomSheet()
-        MiniBarPlayerHandler(this, binding.bottomSheet.miniPlayer, behaviour, bottomSheetHandler::setBottomSheetPeek).setupMiniBar()
+        MiniBarPlayerHandler(
+            this,
+            binding.bottomSheet.miniPlayer,
+            behaviour,
+            bottomSheetHandler::setBottomSheetPeek
+        ).setupMiniBar()
         NowPlayingHandler(this, binding.bottomSheet.nowPlaying).setupNowPlaying()
     }
 
     override fun onDestroy() {
         Log.d("TAG", "onDestroy: Destroying main activity")
-        getMediaRemote()?.release()
+        getMediaRemote().release()
         super.onDestroy()
     }
 }
--- app/src/main/java/app/moosync/moosync/ui/pages/playlists/PlaylistsFragment.kt
+++ app/src/main/java/app/moosync/moosync/ui/pages/playlists/PlaylistsFragment.kt
@@ -1,3 +1,23 @@
 package app.moosync.moosync.ui.pages.playlists
 
-class PlaylistsFragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import app.moosync.moosync.R
+import app.moosync.moosync.databinding.FragmentSongsListBinding
+import app.moosync.moosync.ui.base.BaseFragment
+
+class PlaylistsFragment: BaseFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding: FragmentSongsListBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_songs_list, container, false)
+
+        return binding.root
+    }
+}
